d53891ed4c0a4216a5d4c6ca8952457e195ae615
jinju2828 <jinju2828@gmail.com>
2025-10-02 16:04:36 -0700

    Setting BE WIP

---
+++ Backend/src/db/kysely.provider.ts
@@ -0,0 +1,25 @@
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export interface Database {
+  todos: {
+    id: number;
+    text: string;
+    done: boolean;
+    order: number;
+  };
+}
+
+export const db = new Kysely<Database>({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    }),
+  }),
+});
