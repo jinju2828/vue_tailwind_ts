0de475e475960931acb9d16912daa4755902bd51
jinju2828 <jinju2828@gmail.com>
2025-10-03 18:40:59 -0700

    Home design fixes

--- Frontend/vue-tailwind-ts/src/views/Home.vue
+++ Frontend/vue-tailwind-ts/src/views/Home.vue
@@ -1,6 +1,6 @@
 <!-- frontend/src/views/Home.vue -->
 <template>
-  <div class="p-6 text-center">
+  <div class="p-6 text-center home">
     <h1 class="text-3xl font-bold mb-6">🎉 Welcome to Vue Todos</h1>
 
     <div class="flex gap-4 justify-center">
@@ -23,3 +23,14 @@ function goAbout() {
   router.push("/about");
 }
 </script>
+
+<style scoped>
+.home {
+  min-height: calc(100vh - 64px); /* 네비게이션 바 높이 제외 */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
